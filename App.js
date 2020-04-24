6c11b51236c9461808410675f3522e616d740425
retrez02 <60401725+retrez02@users.noreply.github.com>
2020-04-25 00:27:53 +0200

    ne

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>mahalla</Text>
+
+    <View style={{ padding: 30 }} >
+
+      <View>
+        <TextInput placeholder="Ziel" style={{ borderBottomColor: 'black', borderBottomWidth: 1 }} />
+        <Button title="ADD" />
+      </View>
+
+      <View>
+
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
